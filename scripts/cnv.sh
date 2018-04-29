d6c5ba10a1a3ca2f23af66a6c23837910ff71b67
penhoorn <penhoorn@users.noreply.github.com>
2018-04-29 13:33:24 +0200

    Update cnv.sh

--- scripts/cnv.sh
+++ scripts/cnv.sh
@@ -8,20 +8,19 @@ zscore=4.7
 assembly=GRCh37.p13.genome
 bed_file=bed/685e972e-7457-4dbf-8a33-3b91ae4a8f29.bed
 
-# all gco lists
-list=`ls lists/*.list`
+# all gco files
+list=`ls controls/*.gco`
 
-# loop over every list
-for gco_list in ${list[@]}
+# loop over every gco file (one per target sample)
+for gco_file in ${list[@]}
 do
         # target name
         target=`basename $gco_list .list`
 
         # output files
-        gco_file=controls/target.gco
         gar_file=gar_files/$assembly/$target.gar
-        cnv_file=cnv_files/$assembly/$target.svn3407-filter_z${zscore}.cnv
-        cap_file=cnv_files/$assembly/$target.svn3407-filter_z${zscore}.captured.cnv
+        cnv_file=cnv_files/$assembly/$target-filter_z${zscore}.cnv
+        cap_file=cnv_files/$assembly/$target-filter_z${zscore}.captured.cnv
 
         # run gastructure cnv
         gaStructure cnv \
@@ -30,19 +29,19 @@ do
                 --output=$cnv_file \
                 --base_include=0.01-2.2 \
                 --cnv_exclude=0.75-1.25 \
-                --min_gain_zscore=${zscore} \
-                --min_loss_zscore=${zscore} \
+                --min_gain_zscore=$zscore \
+                --min_loss_zscore=$zscore \
                 --zscore_handling=FILTER \
                 --tab_separator
                 
         # transform cnv output to temporary bed file
-        grep '^chr' $flt_file > $tmp_path/temp.bed
+        grep '^chr' $flt_file > temp.bed
 
         # intersect temp bed with captured regions to get capture only cnvs
-        bedtools intersect -wb -a $bed_file -b $tmp_path/temp.bed \
+        bedtools intersect -wb -a $bed_file -b temp.bed \
                 > $cap_file
 
-        rm $tmp_path/temp.bed
+        rm temp.bed
 
 done
 
